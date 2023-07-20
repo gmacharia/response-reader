0b8f1361faa1fdd8d95edae11be8f3aacb6ecd27
John Macharia <john.macharia@cellulant.io>
2023-07-20 21:03:41 +0300

    add try catch

--- src/main/java/com/example/response/reader/ResponseApplication.java
+++ src/main/java/com/example/response/reader/ResponseApplication.java
@@ -1,22 +1,31 @@
 package com.example.response.reader;
 
 import com.example.response.reader.dao.ResponsePayload;
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ *
+ * @author kobe
+ */
 public class ResponseApplication {
 
     public static void main(String[] args) throws JsonProcessingException {
         String jsonResponse = "{\"data\":{\"id\":\"\",\"status\":\"FAILED\"},\"status\":{\"isSuccess\":false,\"message\":\"Please enter a valid Account Number\",\"errorCode\":\"ERR373\"}}";
 
-        var response = new ObjectMapper().readValue(jsonResponse, ResponsePayload.class);
-        System.out.println(new ObjectMapper().writeValueAsString(response));
+        try {
+            var response = new ObjectMapper().readValue(jsonResponse, ResponsePayload.class);
+            System.out.println(new ObjectMapper().writeValueAsString(response));
 
-        System.out.println("Data - ID: " + response.data.getId());
-        System.out.println("Data - Status: " + response.data.getStatus());
-        System.out.println("Is Success: " + response.status.getIsSuccess());
-        System.out.println("Message: " + response.status.getMessage());
-        System.out.println("Error Code: " + response.status.getErrorCode());
+            System.out.println("Data - ID: " + response.data.getId());
+            System.out.println("Data - Status: " + response.data.getStatus());
+            System.out.println("Is Success: " + response.status.getIsSuccess());
+            System.out.println("Message: " + response.status.getMessage());
+            System.out.println("Error Code: " + response.status.getErrorCode());
+        } catch (JsonParseException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
 }
